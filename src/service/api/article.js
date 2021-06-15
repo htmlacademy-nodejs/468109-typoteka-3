9ae67b6edde69e8eec8a7f462cc2c10d0757ceb4
vitalyionov <vitalyionov@gmail.com>
2021-06-15 10:59:16 +0300

    Правит замечание от наставника

--- src/service/api/article.js
+++ src/service/api/article.js
@@ -13,93 +13,70 @@ module.exports = (app, service) => {
 
   app.use(`/article`, route);
 
-  route.get(
-      `/`,
-      (req, res) => {
-        const articles = service.findAll();
+  route.get(`/`, (req, res) => {
+    const articles = service.findAll();
 
-        return res.status(StatusCodes.OK)
-          .json(articles);
-      });
+    return res.status(StatusCodes.OK)
+      .json(articles);
+  });
 
-  route.post(
-      `/`,
-      entityValidator(articleKeys, entityNames.ARTICLE),
-      (req, res) => {
-        const article = service.create(req.body);
+  route.post(`/`, entityValidator(articleKeys, entityNames.ARTICLE), (req, res) => {
+    const article = service.create(req.body);
 
-        return res.status(StatusCodes.CREATED)
-          .json(article);
-      });
+    return res.status(StatusCodes.CREATED)
+      .json(article);
+  });
 
-  route.get(
-      `/:articleId`,
-      articleExists(service),
-      (req, res) => {
-        const {article} = res.locals;
+  route.get(`/:articleId`, articleExists(service), (req, res) => {
+    const {article} = res.locals;
 
-        return res.status(StatusCodes.OK)
-          .json(article);
-      });
+    return res.status(StatusCodes.OK)
+      .json(article);
+  });
 
-  route.put(
-      `/:articleId`,
-      [articleExists(service), entityValidator(articleKeys, entityNames.ARTICLE)],
-      (req, res) => {
-        const {articleId} = req.params;
+  route.put(`/:articleId`, [articleExists(service), entityValidator(articleKeys, entityNames.ARTICLE)], (req, res) => {
+    const {articleId} = req.params;
 
-        const newArticle = service.update(articleId, req.body);
+    const newArticle = service.update(articleId, req.body);
 
-        return res.status(StatusCodes.OK)
-          .json(newArticle);
-      });
+    return res.status(StatusCodes.OK)
+      .json(newArticle);
+  });
 
-  route.delete(
-      `/:articleId`,
-      articleExists(service),
-      (req, res) => {
-        const {articleId} = req.params;
+  route.delete(`/:articleId`, articleExists(service), (req, res) => {
+    const {articleId} = req.params;
 
-        const deletedArticle = service.drop(articleId);
+    const deletedArticle = service.drop(articleId);
 
-        return res.status(StatusCodes.OK)
-          .json(deletedArticle);
-      });
+    return res.status(StatusCodes.OK)
+      .json(deletedArticle);
+  });
 
-  route.get(
-      `/:articleId/comments`,
-      articleExists(service),
-      (req, res) => {
-        const {article} = res.locals;
+  route.get(`/:articleId/comments`, articleExists(service), (req, res) => {
+    const {article} = res.locals;
 
-        const comments = service.findAllComments(article);
+    const comments = service.findAllComments(article);
 
-        return res.status(StatusCodes.OK)
-          .json(comments);
-      });
+    return res.status(StatusCodes.OK)
+      .json(comments);
+  });
 
-  route.post(
-      `/:articleId/comments/`,
-      [articleExists(service), entityValidator(commentKeys, entityNames.COMMENT)],
-      (req, res) => {
-        const {article} = res.locals;
+  route.post(`/:articleId/comments/`, [articleExists(service), entityValidator(commentKeys, entityNames.COMMENT)], (req, res) => {
+    const {article} = res.locals;
 
-        const newComment = service.createComment(article, req.body);
+    const newComment = service.createComment(article, req.body);
 
-        return res.status(StatusCodes.CREATED)
-          .json(newComment);
-      });
+    return res.status(StatusCodes.CREATED)
+      .json(newComment);
+  });
 
-  route.delete(
-      `/:articleId/comments/:commentId`,
-      [articleExists(service), commentExists(service), entityValidator(commentKeys, entityNames.COMMENT)],
-      (req, res) => {
-        const {article} = res.locals;
-        const {commentId} = req.params;
+  route.delete(`/:articleId/comments/:commentId`, [articleExists(service), commentExists(service), entityValidator(commentKeys, entityNames.COMMENT)], (req, res) => {
+    const {article} = res.locals;
+    const {commentId} = req.params;
 
-        const deletedComment = service.dropComment(article, commentId);
+    const deletedComment = service.dropComment(article, commentId);
 
-        return res.status(StatusCodes.OK)
-          .json(deletedComment);
-      });
+    return res.status(StatusCodes.OK)
+      .json(deletedComment);
+  });
 };
