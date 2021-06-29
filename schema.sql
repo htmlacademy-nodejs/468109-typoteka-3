fc0c12c6cc75134c0063862ead68fba680c97825
vitalyionov <ionovvi@mos.ru>
2021-06-29 10:36:25 +0300

    Исправляет замечания наставника

--- schema.sql
+++ schema.sql
@@ -14,7 +14,7 @@ CREATE TABLE users(
   last_name varchar(255) NOT NULL,
   email varchar(255) NOT NULL UNIQUE,
   password_hash varchar(255) NOT NULL,
-  avatar varchar(50) NOT NULL
+  avatar varchar(255) NOT NULL
 );
 
 CREATE TABLE articles(
@@ -23,10 +23,12 @@ CREATE TABLE articles(
   publication_date date NOT NULL,
   user_id integer NOT NULL,
   announce varchar(250) NOT NULL,
-  photo varchar(50),
+  photo varchar(255),
   full_text varchar(1000),
   created_at timestamp DEFAULT current_timestamp,
   FOREIGN KEY (user_id) REFERENCES users(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
 
 CREATE TABLE comments(
@@ -36,7 +38,11 @@ CREATE TABLE comments(
   text text NOT NULL,
   created_at timestamp DEFAULT current_timestamp,
   FOREIGN KEY (article_id) REFERENCES articles(id),
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
   FOREIGN KEY (user_id) REFERENCES users(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
 
 CREATE TABLE categories(
@@ -49,7 +55,11 @@ CREATE TABLE articles_categories(
   category_id integer NOT NULL,
   PRIMARY KEY (article_id, category_id),
   FOREIGN KEY (article_id) REFERENCES articles(id),
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
   FOREIGN KEY (category_id) REFERENCES categories(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
 
 CREATE INDEX ON articles(title);
